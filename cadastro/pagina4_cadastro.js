e939dbd9994171812a9251f1b82a7ea1791b103a
Kauane Neres da Silva <kauane.silva@prosecurity.com.br>
2025-05-15 15:13:32 -0300

    Direcionamento para o perfil adicionado

--- cadastro/pagina4_cadastro.js
+++ cadastro/pagina4_cadastro.js
@@ -1,26 +1,38 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const cadastroForm = document.getElementById("cadastro-form");
+//Firebase SDK como módulo
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+    import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-analytics.js";
+    import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
 
-    cadastroForm.addEventListener("submit", function (event) {
-        event.preventDefault();
+    // Configuração do seu projeto Firebase
+    const firebaseConfig = {
+      apiKey: "AIzaSyCXp7nqLSFN3UnL-QCGREo_-JClyLosplQ",
+      authDomain: "projeto-integrador-f65ec.firebaseapp.com",
+      projectId: "projeto-integrador-f65ec",
+      storageBucket: "projeto-integrador-f65ec.firebasestorage.app",
+      messagingSenderId: "12669885040",
+      appId: "1:12669885040:web:bc9ec19edc0bf5511a32b5",
+      measurementId: "G-SXG8BE7R1Y"
+    };
 
-        const email = document.getElementById("email").value;
-        const senha = document.getElementById("senha").value;
+    // Inicializar Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+    const auth = getAuth(app);
 
-        createUserWithEmailAndPassword(auth, email, senha)
-            .then((userCredential) => {
-                alert("Conta criada com sucesso!");
-                console.log("Usuário cadastrado:", userCredential.user);
+    // Cadastro de usuário
+    document.getElementById("cadastro-form").addEventListener("submit", function (event) {
+      event.preventDefault();
 
-                // Aguarda a autenticação antes de redirecionar
-                auth.onAuthStateChanged((user) => {
-                    if (user) {
-                        window.location.href = "../pagina_perfil/perfil.html"; // Certifique-se de que o caminho está correto
-                    }
-                });
-            })
-            .catch((error) => {
-                alert("Erro ao cadastrar: " + error.message);
-            });
+      const email = document.getElementById("email").value;
+      const senha = document.getElementById("senha").value;
+
+      createUserWithEmailAndPassword(auth, email, senha)
+        .then((userCredential) => {
+          alert("Conta criada com sucesso!");
+          console.log("Usuário cadastrado:", userCredential.user);
+           window.location.href = "../perfil/perfil.html";
+        })
+        .catch((error) => {
+          alert("Erro ao cadastrar: " + error.message);
+        });
     });
-});
